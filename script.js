77993b1249af8b468d21e62e6c06fbfa6d83d57f
David Kržak <davidkrizak@hotmail.com>
2024-11-19 18:05:13 +0100

    Dodan bg
    Dodan BG za fail i succes text

--- script.js
+++ script.js
@@ -19,26 +19,27 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.log("Random champ je: ", randomChampion);
 
     // svaki champ ima splashart a link za art je npr https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg 
-    //Sufix _0 jer je to base image, sufix _1 je prvi skin _2 drugi itd... Ako champ ima vise od 1 skin
+    // Sufix _0 jer je to base image, sufix _1 je prvi skin _2 drugi itd... Ako champ ima vise od 1 skin
     championImage.src = `${imageBaseUrl}${randomChampion.id}_0.jpg`;
     console.log("Prikazan ", championImage.src);
 
     guessButton.addEventListener('click', () => {
         const userGuess = guessInput.value.trim().toLowerCase();
         console.log("User input:", userGuess);
-
+    
         if (userGuess === randomChampion.name.toLowerCase()) {
             console.log("Correct guess:", randomChampion.name);
-
-            result.textContent = `Točno! Pogodili ste championa ${randomChampion.name}!`;
-            result.classList.add('text-success');
-            result.classList.remove('text-danger');
+    
+            result.textContent = `Točno! Pogodili ste  ${randomChampion.name}!`;
+            result.classList.add('bg-success', 'text-white', 'rounded'); // dodaj klasu za bootstrap da se vidi na pozadini
+            result.classList.remove('bg-danger');
         } else {
-            console.log("Krvo, pravo ime je ", randomChampion.name);
-
+            console.log("Krivo, pravo ime je ", randomChampion.name);
+    
             result.textContent = 'Pokušajte ponovno!';
-            result.classList.add('text-danger');
-            result.classList.remove('text-success');
+            result.classList.add('bg-danger', 'text-white', 'rounded');  // dodaje klasu bootstrapu za fail 
+            result.classList.remove('bg-success');
         }
     });
+    
 });
